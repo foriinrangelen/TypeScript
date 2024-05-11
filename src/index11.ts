69dbb030ca912ac60552ae9dc1db0d3bcdb0a941
김유열 <kyyyy8629@gmail.com>
2024-05-11 23:49:14 +0900

    제네릭 정리 index11

--- src/index11.ts
+++ src/index11.ts
@@ -103,3 +103,266 @@ getLength({ length: 1 }); // ✅
 
 // 호출은 인수로 null을 전달했으므로 오류가 발생(length 프로퍼티가 없음)
 // getLength(null);             // ❌
+
+//  map 메서드에 타입스크립트 적용해보기
+const arr1 = [1, 2, 3];
+const newArr1 = arr1.map((value) => value * 2); // [2,4,6]
+
+function map<T>(arr: T[], callback: (i: T) => T) {
+  let result = [];
+  for (let i = 0; i < arr1.length; i++) {
+    result.push(callback(arr[i]));
+  }
+  return result;
+}
+map(arr1, (it) => it * 2);
+map(["hello", "world"], (it) => it.toUpperCase());
+// map(["hello", "world"], (it) => parseInt(it)); 에러발생 string타입이 들어와서 반환값타입도 string타입으로 추론되기때문
+// 위의 map 함수에서 들어오는 배열에 따라 타입이결정되어야 한다
+// 하지만 꼭 string배열이 들어온다고 return 타입도 string배열이 될필요가 없다 이러한경우
+// 제네릭타입변수를 하나만사용하면안되고 두개이상의 타입변수를사용해야한다
+function map1<T, U>(arr: T[], callback: (item: T) => U) {
+  let result = [];
+  for (let i = 0; i < arr1.length; i++) {
+    result.push(callback(arr[i]));
+  }
+  return result;
+}
+map1(["hello", "world"], (it) => parseInt(it));
+// 이러한 경우에서는 string 배열타입이 들어와서 T에 string[] 타입할당, callback함수의 매개변수 타입도 string타입이되지만
+// 콜백함수의 return타입은 (it) => parseInt(it) 이때 number타입으로 추론이 되어서 U에 number타입이 들어온다
+
+// forEach 메서드에 타입스크립트 적용해보기
+const arr2 = [1, 2, 3];
+arr2.forEach((it) => console.log(it));
+
+function forEach<T>(arr: T[], callback: (item: T) => void) {
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i]);
+  }
+}
+forEach(arr2, (it) => console.log(it.toFixed()));
+forEach(["hello", "world"], (it) => console.log(it.toUpperCase()));
+
+// 제네릭 인터페이스와 제네릭 타입별칭
+
+/**
+ * 제네릭 인터페이스
+ */
+interface KeyFair<K, V> {
+  key: K;
+  value: V;
+}
+
+// 제네릭 인터페이스는 제네릭함수와 다르게 타입으로 정의할때 반드시 꺽쇠를 열고 타입을 직접할당 해주어야 한다
+let keyFair: KeyFair<string, number> = {
+  key: "hello",
+  value: 0,
+};
+
+let keyFair2: KeyFair<boolean, string[]> = {
+  key: true,
+  value: ["hello", "world"],
+};
+
+// 이렇게 하나의 제네릭 인터페이스로 여러개의 객체를 정의할 수 있다
+
+// 제네릭인터페이스와 객체의 인덱스시그니처를 활용하면 굉장히 유연한 객체타입을 만들 수 있다
+// 인덱스 시그니처란?
+interface NumberMap {
+  [key: string]: number; // 와같이 key와 value의 규칙만 만족하면 어떤객체든 허용하는 유연한 문법
+}
+// 예시
+let numberMap1: NumberMap = {
+  key: 1234,
+  ket2: 12345,
+};
+
+// 제네릭+ 인덱스시그니처 활용한 인터페이스
+interface Map<V> {
+  [key: string]: V;
+}
+
+let stringMap: Map<string> = {
+  key: "value",
+  key2: "value2",
+};
+
+let booleanMap: Map<boolean> = {
+  key: true,
+};
+// 와같이 유연하게 활용가능
+
+// 제네릭 타입별칭(제네릭 인터페이스와 문법만 다르지 거의 동일)
+type Map2<V> = {
+  [key: string]: V;
+};
+
+let stringMap2: Map2<string> = {
+  key: "value",
+};
+
+/**
+ * 제네릭 인터페이스의 활용 예시
+ * -> 유저 관리 프로그램
+ * -> 유저구분: 학생유저 / 개발자 유저
+ */
+
+interface Student {
+  type: "student";
+  school: string;
+}
+
+interface Developer {
+  type: "developer";
+  skill: string;
+}
+
+interface User {
+  name: string;
+  profile: Student | Developer;
+}
+
+const developerUser: User = {
+  name: "김유열",
+  profile: {
+    type: "developer",
+    skill: "typescript",
+  },
+};
+
+const studentUser: User = {
+  name: "김유열",
+  profile: {
+    type: "student",
+    school: "",
+  },
+};
+
+function goToSchool(user: User) {
+  if (user.profile.type === "student") {
+    console.log(user.profile.school);
+  } else {
+    console.log(user.profile.skill);
+  }
+}
+interface Student {
+  type: "student";
+  school: string;
+}
+
+interface Developer {
+  type: "developer";
+  skill: string;
+}
+
+// 와같이도 타입가드를 통해 좁힐 수 있지만 제네릭을 활용하여 아래처럼 가능하다
+interface User1<T> {
+  name: string;
+  profile: T;
+}
+const developerUser1: User1<Developer> = {
+  name: "김유열",
+  profile: {
+    type: "developer",
+    skill: "typescript",
+  },
+};
+
+const studentUser1: User1<Student> = {
+  name: "김유열",
+  profile: {
+    type: "student",
+    school: "",
+  },
+};
+
+function goToSchool1(user: User1<Student>) {
+  const school = user.profile.school;
+  console.log(`${school}로 등교완료`);
+}
+// 같이 제네릭을 활용하여 타입좁히기를 하지않아도 된다
+
+/**
+ * 제네릭 클래스 알아보기
+ */
+
+class NumnerList {
+  constructor(private list: number[]) {}
+
+  push(data: number) {
+    this.list.push(data);
+  }
+  pop() {
+    return this.list.pop();
+  }
+  print() {
+    console.log(this.list);
+  }
+}
+const numberList = new NumnerList([1, 2, 3]);
+numberList.pop();
+numberList.push(4);
+numberList.print();
+// stringList를 만들려면? 반복작업이 들어가기때문에 제네릭으로 해결
+class List<T> {
+  constructor(private list: T[]) {}
+
+  push(data: T) {
+    this.list.push(data);
+  }
+  pop() {
+    return this.list.pop();
+  }
+  print() {
+    console.log(this.list);
+  }
+}
+const stringList = new List(["hello", "world"]);
+stringList.pop();
+stringList.push("TS");
+stringList.print();
+
+/**
+ * 프로미스와 제네릭
+ */
+// 비동기 결과의 값이 number임을 제네릭문법으로 알려줌
+const promise = new Promise<number>((resolve, reject) => {
+  setTimeout(() => {
+    // resolve(20);
+    reject("~~때문에 실패"); //선택적 any타입으로 추론되기때문에 아무거나 넣어도 된다
+  }, 3000);
+});
+promise.then((result) => {
+  console.log(result * 10); //20
+});
+promise.catch((err) => {
+  //   console.log(err);
+  if (typeof err === "string") console.log(err); // 와같이 주로 사용
+});
+// 정리 : 비동기 결과값인 resolve의 값의 타입은 제네릭으로 정해줄 수 있지만 reject의 값은 지정해줄 수 없다(any로 알아서 추론된다)
+
+// 프로미스를 반환하는 함수의 타입을 정의해보기
+interface Post {
+  id: number;
+  title: string;
+  content: string;
+}
+// 함수의 반환값으로 Promise<Post>해주든 프로미스 자체에 <Post> 해주든 둘다 사용가능하지만 주로 함수의 반환값으로 Promise<Post>하는걸 추천
+// 협업관점에서 함수의 리턴값을 바로 알아보기가 쉬움
+function fetchPost(): Promise<Post> {
+  return new Promise<Post>((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        id: 1,
+        title: "게시글 제목",
+        content: "게시글 컨텐츠",
+      });
+    }, 5000);
+  });
+}
+
+const postRequest = fetchPost();
+postRequest.then((post) => {
+  post.id;
+});
